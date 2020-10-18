f0338b2798c19f619f4374487b9ef168357f8db3
Nick Gerancher <nick@nicksdesk.com>
2020-10-18 15:43:15 -0400

    Initial Commit

---
+++ Insultr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insultr
+//
+//  Created by Nick Gerancher on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
